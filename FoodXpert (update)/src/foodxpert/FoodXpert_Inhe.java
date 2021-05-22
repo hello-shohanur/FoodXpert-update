a04254a141e40cd7939fb6bee3f7c1b363eea304
Md Shohanur Rahman <70064209+shohan3401@users.noreply.github.com>
2021-05-22 15:29:26 +0600

    Add files via upload

---
+++ FoodXpert (update)/src/foodxpert/FoodXpert_Inhe.java
@@ -0,0 +1,17 @@
+package foodxpert;
+
+public class FoodXpert_Inhe extends FoodXpert_Inhe_p{
+    
+    String sn5;
+    void fx_show(){
+        System.out.println(sn3);
+        System.out.println(sn4);
+        System.out.println(sn5);
+    }
+    void poly_show(){
+        System.out.println("Americano");
+        System.out.println("Espresso");
+        System.out.println("Doppio");
+    }
+        
+}
---
+++ FoodXpert (update)/src/foodxpert/FoodXpert_Inhe_p.java
@@ -0,0 +1,17 @@
+
+package foodxpert;
+
+public class FoodXpert_Inhe_p{
+    
+    String sn1, sn2, sn3, sn4;
+    
+    void fx1_show(){
+        System.out.println(sn1);
+        System.out.println(sn2);
+    }
+    void poly_show(){
+        System.out.println("Latte");
+        System.out.println("Cappuccino");
+        
+    }
+}
